926e78d7631e625ba77326da909933485f8326f3
Hanna Yang <hyang6@stuy.edu>
2018-04-11 18:43:00 -0400

    added new files and basic setup for them

---
+++ UserOfMazeSolver.java
@@ -0,0 +1,9 @@
+public class UserOfMazeSolver { 
+
+    public static void main( String[] args) {
+        Maze maze = new Maze( commandLine[0]
+                            , Integer.parseInt( commandLine[1])
+                            , Integer.parseInt( commandLine[2])
+                            );
+    }
+}
